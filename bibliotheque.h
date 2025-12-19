617caac57a06363c78a3d0c3caa95a05a23ba150
ndampofouramichaelyves-source <ndampofouramichaelyves@gmail.com>
2025-12-19 12:06:06 +0100

    bibliotheque.h

--- bibliotheque.h
+++ bibliotheque.h
@@ -1,1 +1,32 @@
+#ifndef __MY_MATH_LIB_H
+#define __MY_MATH_LIB_H
 
+// Fonctions originales
+unsigned int fact(unsigned int value);
+int power(int value, unsigned int pow);
+
+// Exercice 13 - Permutation
+void permut(int *a, int *b);
+
+// Exercice 7 - Équation du second degré
+typedef struct {
+    double x1;
+    double x2;
+    int nb_solutions; // -1: infinité, 0: aucune, 1: une, 2: deux
+} SolutionSecondDegre;
+
+SolutionSecondDegre resoudreEquationSecondDegre(double a, double b, double c);
+void afficherSolutionSecondDegre(SolutionSecondDegre sol);
+
+// Exercice 12a - Nombre premier
+int estPremier(unsigned int n);
+
+// Exercice 13 - Structure Point et distance
+typedef struct {
+    double x;
+    double y;
+} Point;
+
+double distancePoints(Point p1, Point p2);
+
+#endif
